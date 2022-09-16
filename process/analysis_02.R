3a3d7a5950c4863fd62a2f66f0e22ded38cc3135
Juan L. Bretón, PMP <jlbreton@exatec.tec.mx>
2022-09-15 19:27:27 -0500

    Fechas ajustadas en textos de la gráfica a junio.

--- process/analysis_02.R
+++ process/analysis_02.R
@@ -41,10 +41,10 @@ df_05 %>%
         axis.line = element_blank(),
         axis.ticks = element_blank()) +
   labs(title = "Evolución de la Violencia Homicida en México",
-       subtitle = "Víctimas de homicidos dolosos acumuladas por mes en cada Entidad Federativa, de enero de 2016 a marzo de 2022.",
+       subtitle = "Víctimas de homicidos dolosos acumuladas por mes en cada Entidad Federativa, de enero de 2016 a junio de 2022.",
        x = NULL,
        y = NULL,
-       caption = "Fuente: Reportes de Incidencia Delictiva al mes de marzo
+       caption = "Fuente: Reportes de Incidencia Delictiva al mes de junio
          2022; Gobierno de México.<br>
          Visualización: Juan L. Bretón, PMP | @BretonPmp",
        fill = "Víctimas de Homicidios por Mes") +
@@ -57,7 +57,7 @@ be03 +
              size = 3, color = "darkgrey") +
   guides(alpha = guide_legend("Inicio de Sexenio", 
                               label = FALSE,
-                              title.vjust =  -0.01)) -> be03
+                              title.vjust =  -0.01)) #-> be03
 
 
-ggsave(filename = "be03", plot = be03, path = "graficas", device = "tiff")
+# ggsave(filename = "be03", plot = be03, path = "graficas", device = "tiff")
