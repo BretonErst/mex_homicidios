b545c9a80e8632cf10f46ae6fc66e35c414658f2
Juan L. Bretón, PMP <jlbreton@exatec.tec.mx>
2023-01-22 20:56:15 -0600

    Plots by rate added.

--- process/analysis_02_estados.R
+++ process/analysis_02_estados.R
@@ -9,6 +9,7 @@
 ## Librerías
 library(tidyverse)
 library(ggtext)
+library(sf)
 library(slider)
 
 
@@ -51,7 +52,8 @@ df_05 %>%
          x = NULL,
          y = NULL,
          caption = "Fuente: Reportes de Incidencia Delictiva
-           2022; Secretariado Ejecutivo del Sistema Nacional de Seguridad Pública, Gobierno de México.<br>
+           2022; Secretariado Ejecutivo del Sistema Nacional de Seguridad Pública, 
+           Gobierno de México.<br>
            Visualización: Juan L. Bretón, PMP | @BretonPmp",
          fill = "Víctimas de Homicidios por Mes") +
     scale_y_discrete(expand = c(0, 0)) +
@@ -70,6 +72,108 @@ be03 +
 # plotly::ggplotly(be03, tooltip = "texto")
 
 # ggsave(filename = "be03", plot = be03, path = "graficas", device = "tiff")
+
+
+## gráfica de calor por tasas
+# carga archivo de población por estado
+nal_estados <- readxl::read_excel("raw/pob_mex.xlsx",
+                                  sheet = "muni",
+                                  trim_ws = TRUE) %>% 
+  janitor::clean_names() %>% 
+  select(estado = entidad_federativa,
+         pob = poblacion_total_2020) %>% 
+  mutate(estado = str_to_upper(estado)) %>% 
+  mutate(estado = if_else(estado == "ESTADO DE MÉXICO", "MÉXICO", estado))
+
+# integrar columna de población con archivo de datos original
+nal_tasa <- df_05 %>% 
+  filter(año > 2019) %>% 
+  left_join(nal_estados, by = "estado") %>% 
+  mutate(tasa_mes = homicidios / pob * 1e5) %>% 
+  select(-año)
+
+
+# visualización
+nal_tasa %>% 
+  na.omit() %>% 
+  mutate(estado = factor(estado, 
+                         levels = rev(sort(unique(estado))))) %>%
+  ggplot(aes(x = fecha,
+             y = estado, 
+             fill = tasa_mes)) +
+    geom_tile(color = "grey70", 
+              alpha = 0.85) +
+    scale_fill_gradient(low = "#FFFFFF", 
+                        high = "#C0392B",
+                        expand = c(0,0)) +
+    theme(text = element_text(family = "Encode Sans Condensed"),
+          plot.caption = element_markdown(color = "grey45", hjust = 0),
+          plot.title = element_text(size = 18, face = "bold"),
+          plot.title.position = "plot",
+          plot.caption.position = "plot",
+          plot.background = element_blank(),
+          panel.background = element_rect(fill = "#FFFFFF"),
+          legend.position = "top",
+          legend.title = element_text(size = 10),
+          legend.text = element_text(size = 8),
+          axis.text.x = element_text(size = 8),
+          axis.line = element_blank(),
+          axis.ticks = element_blank()) +
+    labs(title = "¿En Qué Estados Ocurren, Proporcionalmente, Más Homicidios?",
+         subtitle = "Víctimas de homicidio doloso por cada 100,000 habitantes por mes",
+         x = NULL,
+         y = NULL,
+         caption = "Fuente: Reportes de Incidencia Delictiva
+             2022; Secretariado Ejecutivo del Sistema Nacional de Seguridad Pública, 
+             Gobierno de México. INEGI, Censo de Población y Vivienda 2020<br>
+             Visualización: Juan L. Bretón, PMP | @BretonPmp",
+         fill = "Proporción de víctimas respecto a la población") +
+    scale_y_discrete(expand = c(0, 0)) +
+    scale_x_date(expand = c(0, 0))
+
+
+## mapa por tasa 2022
+# carga de archivo shape
+nal_mapa <- st_read("conjunto_de_datos/nal/00ent.shp")
+
+# summarize para total año 2020
+nal_tasa_2022 <- nal_tasa %>% 
+  filter(fecha >= "2022-01-01") %>% 
+  group_by(id_entidad, estado, pob) %>% 
+  summarize(total_2022 = sum(homicidios, na.rm = TRUE)) %>% 
+  mutate(tasa_2022 = total_2022 / pob * 1e5) %>% 
+  select(-c(pob, total_2022))
+
+# unir con el archivo shape
+nal_mapa_2022 <- nal_mapa %>% 
+  left_join(nal_tasa_2022, by = c("CVEGEO" = "id_entidad"))
+
+# visualización de mapa
+nal_mapa_2022 %>% 
+  ggplot(aes(fill = tasa_2022)) +
+    geom_sf(color = "grey60",
+            size = 0.7) +
+    theme(text = element_text(family = "Encode Sans Condensed"),
+          plot.title = element_text(face = "bold", 
+                                    size = 16),
+          panel.background = element_rect(fill = "#FFFFFF"),
+          panel.grid = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          axis.line = element_blank(),
+          plot.caption = element_markdown(color = "darkgrey",
+                                          hjust = 0),
+          plot.title.position = "plot",
+          plot.caption.position = "plot",) +
+    labs(title = "Patrones de Violencia Homicida",
+         subtitle = "Víctimas de homicidio por 100,000 habitantes en 2022",
+         caption = "Fuente: Reportes de Incidencia Delictiva
+             2022; Secretariado Ejecutivo del Sistema Nacional de Seguridad Pública, 
+             Gobierno de México.<br>
+             Visualización: Juan L. Bretón, PMP | @BretonPmp") +
+    scale_fill_gradient(name = "Tasa de\nvíctimas", 
+                        low = "#FFFFFF",
+                        high = "#C0392B")
 
 
 # slider
